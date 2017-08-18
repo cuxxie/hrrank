de03d79e5072a4e0e0f3c5081bd6256fdb94297e
Hendri <hendri.wang88@gmail.com>
2017-08-18 22:58:08 +0700

    initial commit

---
+++ gmsach/cmake-build-debug/CMakeFiles/gmsach.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gmsach.dir/main.cpp.obj"
+  "gmsach.pdb"
+  "gmsach.exe"
+  "gmsach.exe.manifest"
+  "libgmsach.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gmsach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
