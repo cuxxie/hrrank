f1dfa47f5bb3f6f9a162c3b16b8b2ea181b5a09d
Hendri <hendri.wang88@gmail.com>
2017-08-18 23:16:45 +0700

    no message

--- gmsach/main.cpp
+++ gmsach/main.cpp
@@ -25,8 +25,27 @@ using namespace std;
 
 int main() {
     //std::cout << "Hello, World!" << std::endl;
-    int a;
-    cin>>a;
-//    cout<<a<<endl;
+    int q;
+    cin>>q;
+    for(int i=0;i<q;i++)
+    {
+        int n,k,x,d;
+        cin>>n;
+        cin>>k;
+        cin>>x;
+        cin>>d;
+        vector<int> pi(n);
+        float total = 0;
+        for(int j=0;j<n;j++){
+            cin>>pi[j];
+            total += max((float)k,(float)(pi[j]*x/100));
+        }
+        float a = total;
+        float b = (float)d;
+        if(a<=b)
+            cout<<"fee"<<endl;
+        else
+            cout<<"upfront"<<endl;
+    }
     return 0;
 }
